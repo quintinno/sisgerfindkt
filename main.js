6616465aed2539de8ad9d96679db9d9e8f9af850
quintinno <josequintinno@outlook.com.br>
2022-12-26 20:32:29 -0300

    :low_brightness: Initial: Criação do Projeto

--- main.js
+++ main.js
@@ -1,16 +1,23 @@
-const {app, BrowserWindow} = require('electron');
+const { app, BrowserWindow } = require('electron');
 const url = require('url');
 const path = require('path');
 
-function onReady () {
-	win = new BrowserWindow({width: 900, height: 6700})
+function onReady() {
+	win = new BrowserWindow({
+		width: 800,
+		height: 500,
+		frame: false,
+		titleBarStyle: 'hidden',
+		// titleBarOverlay: {
+		// 	color: '#2f3241',
+		// 	symbolColor: '#74b1be',
+		// 	height: 20
+		// }
+	});
+	win.setMenuBarVisibility(false);
 	win.loadURL(url.format({
-		pathname: path.join(
-			__dirname,
-			'dist/sisgerfindkt/index.html'),
-		protocol: 'file:',
-		slashes: true
-	}))
+		pathname: path.join(__dirname, '/dist/sisgerfindkt/index.html'), protocol: 'file:', slashes: true,
+	}));
 }
 
 app.on('ready', onReady);
